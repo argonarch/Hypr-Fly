d62e18001aeee2a4223b6a629622289da8f89c10
Armando <Argonarch@localhost.localdomain>
2023-04-06 14:36:38 -0300

    add item layout

--- node_2d.gd
+++ node_2d.gd
@@ -4,10 +4,10 @@ extends Node2D
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	get_viewport().transparent_bg = true;
-	
-func _process(delta):
-	#$CharacterBody2D/Line2D.set_point_position(1, Vector2(0,0))
-	#$CharacterBody2D/Line2D.set_point_position(0, $pricipal.get_position())
-	
-	pass
-
+	var items = []
+	var num_items = 4
+	for num in range(0,num_items):
+		print(num)
+		items.append($"item-area".duplicate(4))
+		add_child(items[num])
+		items[num].position = $pricipal.get_global_position() + Vector2(200, 0).rotated(deg_to_rad((360.0/num_items)*num))
