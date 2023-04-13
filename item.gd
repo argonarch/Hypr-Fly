930e0c9e55c080a147cadf3f0ba947083df31b72
Armando <Argonarch@localhost.localdomain>
2023-04-13 17:02:21 -0300

    add function execute and collider sector

--- item.gd
+++ item.gd
@@ -1,20 +1,71 @@
-extends CharacterBody2D
-
+extends Area2D
 
 var can_grab = false
 var grabbed_offset = Vector2()
+var db : SQLite = null
+var db_name := "res://DataStore/database"
 
-func _input_event(viewport, event, shape_idx):
+func _ready():
+	db = SQLite.new()
+	db.path = db_name
+	area_entered.connect(_on_area_entered)
+	area_exited.connect(_on_area_exited)
+	input_event.connect(_on_input_event)
+	mouse_entered.connect(_on_mouse_entered)
+	get_node("../Principal/neutral").area_exited.connect(_on_neutral_area_exited)
+	get_node("../Principal/neutral").area_entered.connect(_on_neutral_area_entered)
+	pass
+
+
+func _on_input_event(viewport, event, shape_idx):
 	if event is InputEventMouseButton:
 		can_grab = event.pressed
 		grabbed_offset = position - get_global_mouse_position()
+		
 
 func _process(delta):
-	if (Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT) or Input.is_mouse_button_pressed(MOUSE_BUTTON_RIGHT)) and can_grab:
-		position = get_global_mouse_position() + grabbed_offset
+	if can_grab:
+		position = get_global_mouse_position()
 	$Line2D.set_point_position(0, Vector2(0,0))
-	$Line2D.set_point_position(1, to_local(get_node("../pricipal").get_global_position()))
+	$Line2D.set_point_position(1, to_local(get_node("../Principal").get_global_position()))
 	pass
 
-func _on_input_event(viewport, event, shape_idx):
-	pass # Replace with function body.
+func _on_area_entered(area):
+	if can_grab:
+		print(str('estas en el area: ', area.get_name()))
+	pass
+
+func _on_area_exited(area):
+	if area.get_name() == "accion" and can_grab == true:
+		db.open_db()
+		var frase = "select * from items where item = '" + get_name() + "' ;"
+		var pregunta = db.query(frase)
+		var command = db.query_result[0]["command"]
+		print("El commando es: " + command)
+		OS.create_process(command, [])
+		print(get_name())
+		db.close_db()
+		#for member in get_tree().get_nodes_in_group("items"):
+		#	member.hide()
+		#	member.queue_free()
+	#print(str('se entro al area: ', area.get_name()))
+	pass 
+
+func _on_mouse_entered():
+	print("hola")
+	can_grab = true
+	#grabbed_offset = position - get_global_mouse_position()
+	pass
+
+
+func _on_neutral_area_exited(area):
+	print("salio del area neutral")
+	if can_grab:
+		$Line2D.visible = true
+	print("se ve linea")
+	pass
+
+func _on_neutral_area_entered(area):
+	if can_grab:
+		$Line2D.visible = false
+	pass
--- node_2d.gd
+++ node_2d.gd
@@ -1,13 +1,43 @@
 extends Node2D
 
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	get_viewport().transparent_bg = true;
 	var items = []
+	var sectors = []
 	var num_items = 4
+	$item.process_mode = 4
+	$item.visible = false
+	$item/Line2D.visible = false
+	$Principal.visible = false
+	$Principal.position = get_global_mouse_position()
+	$Principal.visible = true
+	var rad_mid = PI/num_items
+	var mid_item : float = num_items/2.0
+	var radian_mid = rad_mid*2
+	var diametro_sector = 250
 	for num in range(0,num_items):
-		print(num)
-		items.append($"item-area".duplicate(4))
+		var radian_start = PI*(num)/mid_item
+		var radian_end = PI*(num+1)/mid_item
+		var radian_center = radian_end/2+num*(PI/mid_item)/2
+		var vectores = PackedVector2Array([Vector2(0,0),Vector2(diametro_sector,0).rotated(radian_start+radian_mid),Vector2(diametro_sector,0).rotated(radian_center+radian_mid),Vector2(diametro_sector,0).rotated(radian_end+radian_mid)])
+		
+		items.append($item.duplicate(4))
 		add_child(items[num])
-		items[num].position = $pricipal.get_global_position() + Vector2(200, 0).rotated(deg_to_rad((360.0/num_items)*num))
+		items[num].set_name("item-" + str(num+1))
+		items[num].position = $Principal.get_global_position() + Vector2(120, 0).rotated(radian_start+rad_mid)
+		items[num].process_mode = 0
+		items[num].add_to_group("items")
+		items[num].visible = true
+		
+		sectors.append($Principal/sector.duplicate(4))
+		$Principal.add_child(sectors[num])
+		sectors[num].set_name("sector-" + str(num+1))
+		sectors[num].get_node("sector").set_polygon(vectores)
+		sectors[num].get_node("sector-collision").set_polygon(vectores)
+		"""
+		print(num)
+		print(mid_item)
+		print(radian_end)
+		print(radian_center)
+		print(rad_to_deg(radian_center))
+		"""
