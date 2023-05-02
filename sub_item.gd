2645c7364f91ae372903c53cbbf854bb546f692b
Armando <Argonarch@localhost.localdomain>
2023-05-02 13:42:29 -0300

    Tue May  2 01:42:29 PM -03 2023

--- sub_item.gd
+++ sub_item.gd
@@ -6,23 +6,34 @@ var db_name := "res://DataStore/database"
 var init_position
 var member_sector
 var sector_status = false
+var instance_fly = load("res://fly.tscn")
 
 func _ready():
 	db = SQLite.new()
 	db.path = db_name
 	area_exited.connect(_on_area_exited)
-	get_node("../item/neutral").mouse_exited.connect(_on_neutral_mouse_exited)
-	get_node("../item/neutral").mouse_entered.connect(_on_neutral_mouse_entered)
+	get_node("../neutral").mouse_exited.connect(_on_neutral_mouse_exited)
+	get_node("../neutral").mouse_entered.connect(_on_neutral_mouse_entered)
 
 func _process(delta):
 	if can_grab:
-		position = get_global_mouse_position()
+		position = get_node("..").get_local_mouse_position()
 	$Line2D.set_point_position(0, Vector2(0,0))
 	$Line2D.set_point_position(1, to_local(get_node("../item").get_global_position()))
 
 func _on_area_exited(area):
 	#print(str('saliste del area: ', area.get_name()))
-	if area.get_name() == "accion" and can_grab == true:
+	if area.get_name() == "accion" and can_grab:
+		can_grab = false
+		for sector in get_tree().get_nodes_in_group("sectors"):
+			sector.queue_free()
+		for sub_item in get_tree().get_nodes_in_group("sub_items"):
+			sub_item.queue_free()
+		get_node("../neutral").queue_free()
+		get_node("../accion").queue_free()
+		var ins_fly = instance_fly.instantiate()
+		get_node("/root").add_child(ins_fly)
+		"""
 		db.open_db()
 		var frase = "select * from items where item = '" + get_name() + "' ;"
 		var pregunta = db.query(frase)
@@ -32,7 +43,7 @@ func _on_area_exited(area):
 		OS.create_process(command, [])
 		print(get_name()) 
 		get_tree().quit()
-
+		"""
 func initial_variable():
 	for member in get_tree().get_nodes_in_group("sectors"):
 		if member.get_name() == "sector-"+num_of_name():
